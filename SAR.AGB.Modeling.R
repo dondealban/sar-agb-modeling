f28083f4c61100731193f6ece6856d754e7b30e4
Don De Alban <dondealban@gmail.com>
2017-03-04 22:28:28 +0800

    Created repository; R project and script

---
+++ SAR.AGB.Modeling.R
@@ -0,0 +1,7 @@
+# This script for modeling synthetic aperture radar backscatter from JAXA's L-band
+# ALOS/PALSAR and ALOS/PALSAR-2 sensors and aboveground biomass data collected from
+# forest inventory plots in the Philippines.
+#
+# Script By:      Jose Don T De Alban
+# Date Created:   04 Mar 2017
+# Last Modified:  
