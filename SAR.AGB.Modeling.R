cf269f04d5950e891445e0899bbb953344e5a4d0
Don De Alban <dondealban@gmail.com>
2017-03-05 07:49:04 +0800

    Computed test logarithmic regression

--- SAR.AGB.Modeling.R
+++ SAR.AGB.Modeling.R
@@ -12,7 +12,20 @@ setwd("/Users/dondealban/Dropbox/Research/sar-agb-modeling/")
 
 
 # LOAD LIBRARIES AND DATA
+library(ggplot2)
 
 # Read data, define variables, and store data in variables
 dataVAM <- read.csv(file="/Users/dondealban/Dropbox/Research/sar-agb-modeling/csv/VAM_1ha_ForR.csv", header=TRUE, sep=",")
+
+
+# LOGARITHMIC REGRESSION
+
+# 2007
+lmHH07AGB <- lm(log(T_HA_50x50) ~ HH07, data=dataVAM)
+lmHV07AGB <- lm(log(T_HA_50x50) ~ HV07, data=dataVAM)
+lmDUAL07AGB <- lm(log(T_HA_50x50) ~ HH07 + HV07, data=dataVAM)
+
+# All Years: 2007, 2008, 2009, 2010, 2015
+lmSARAGB <- lm(log(T_HA_50x50) ~ HH07+HV07+HH08+HV08+HH09+HV09+HH10+HV10+HH15+HV15, data=dataVAM)
+
 
