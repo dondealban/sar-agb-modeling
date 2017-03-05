ef41f7d6cea91e9958cff0354a3abcb233de063a
Don De Alban <dondealban@gmail.com>
2017-03-05 08:11:58 +0800

    Calculate log regression, prediction; save results to files

--- SAR.AGB.Modeling.R
+++ SAR.AGB.Modeling.R
@@ -29,3 +29,42 @@ lmDUAL07AGB <- lm(log(T_HA_50x50) ~ HH07 + HV07, data=dataVAM)
 lmSARAGB <- lm(log(T_HA_50x50) ~ HH07+HV07+HH08+HV08+HH09+HV09+HH10+HV10+HH15+HV15, data=dataVAM)
 
 
+# PREDICTION
+
+pred07 <- predict(lmDUAL07AGB)
+predALL <- predict(lmSARAGB)
+
+
+# SAVE RESULTS TO FILE
+
+# Print regression summaries as txt file
+
+sink("output-vam-sar-agb-lm-2007.txt", append=FALSE, split=TRUE)
+print("Logarithmic Regression: 2007 SAR Gamma0 Backscatter vs Aboveground Biomass")
+print(summary(lmHH07AGB))
+print(summary(lmHV07AGB))
+print(summary(lmDUAL07AGB))
+sink()
+
+sink("output-vam-sar-agb-lm-allyears.txt", append=FALSE, split=TRUE)
+print("Logarithmic Regression: All Years SAR Gamma0 Backscatter vs Aboveground Biomass")
+print(summary(lmSARAGB))
+sink()
+
+# Generate regression plots
+
+# 2007
+saragb07 <- ggplot(dataVAM, aes(x=T_HA_50x50, y=exp(pred07)))
+saragb07 <- saragb07 + geom_point() + stat_smooth(method="lm", se=TRUE) + theme_bw()
+saragb07 <- saragb07 + labs(title="2007: Field vs Predicted AGB", x="Field AGB(t/ha)", y="Predicted AGB (t/ha)")
+
+# All Years
+saragbALL <- ggplot(dataVAM, aes(x=T_HA_50x50, y=exp(predALL)))
+saragbALL <- saragbALL + geom_point() + stat_smooth(method="lm", se=TRUE) + theme_bw()
+saragbALL <- saragbALL + labs(title="All Years: Field vs Predicted AGB", x="Field AGB(t/ha)", y="Predicted AGB (t/ha)")
+
+# Save regression plots
+
+ggsave(saragb07,  file="plot-logres-2007.pdf", width=7, height=5.5, units="in", dpi=300)
+ggsave(saragbALL, file="plot-logres-allyears.pdf", width=7, height=5.5, units="in", dpi=300)
+
